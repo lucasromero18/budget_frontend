8ab01b889509175f54d6fc4e68542c68883546eb
Lucas Romero <lucasromero65@yahoo.com>
2019-05-27 18:25:33 -0700

    landing page done... kinda

--- src/Components/LandingPage.js
+++ src/Components/LandingPage.js
@@ -22,7 +22,7 @@ class LandingPage extends Component {
           <Button className="hero-landing-page-button" size="lg"><a className="hero-landing-page-button-anchor" href="#three-reasons-landing-page">Why BudgetCity?</a></Button>
         </div>
         <div id="three-reasons-landing-page">
-          <h1 className="three-reasons-header">The Purpose</h1>
+          <h1 className="three-reasons-header-purpose">Why track your expenses?</h1>
           <div className="three-reasons-paragraph-container">
             <div className="three-reasons-paragraph-text">"Lorem ipsum dolor sit amet, consectetur adipiscing 
               elit, sed do eiusmod tempor incididunt ut labore et 
@@ -32,7 +32,40 @@ class LandingPage extends Component {
               reprehenderit in voluptate velit esse cillum dolore 
               u fugiat nulla pariatur. Excepteur sint occaecat 
               cupidatat non proident, sunt in culpa qui officia 
-              deserunt mollit anim id est laborum."</div>
+              deserunt mollit anim id est laborum."
+            </div>
+          </div>
+          <div className="three-reasons-arrow-container">
+            <i class="fas fa-arrow-down"></i>
+          </div>
+          <h1 className="three-reasons-header">Here's why BudgetCity is special</h1>
+          <div className="three-reasons-paragraph-container">
+            <div className="three-reasons-paragraph-text">"Lorem ipsum dolor sit amet, consectetur adipiscing 
+              elit, sed do eiusmod tempor incididunt ut labore et 
+              dolore magna aliqua. Ut enim ad minim veniam, quis
+              nostrud exercitation ullamco laboris nisi ut aliquip
+              ex ea commodo consequat. Duis aute irure dolor in 
+              reprehenderit in voluptate velit esse cillum dolore 
+              u fugiat nulla pariatur. Excepteur sint occaecat 
+              cupidatat non proident, sunt in culpa qui officia 
+              deserunt mollit anim id est laborum."
+            </div>
+          </div>
+          <div className="three-reasons-arrow-container">
+            <i class="fas fa-arrow-down"></i>
+          </div>
+          <h1 className="three-reasons-header">What's in it for you?</h1>
+          <div className="three-reasons-paragraph-container">
+            <div className="three-reasons-paragraph-text">"Lorem ipsum dolor sit amet, consectetur adipiscing 
+              elit, sed do eiusmod tempor incididunt ut labore et 
+              dolore magna aliqua. Ut enim ad minim veniam, quis
+              nostrud exercitation ullamco laboris nisi ut aliquip
+              ex ea commodo consequat. Duis aute irure dolor in 
+              reprehenderit in voluptate velit esse cillum dolore 
+              u fugiat nulla pariatur. Excepteur sint occaecat 
+              cupidatat non proident, sunt in culpa qui officia 
+              deserunt mollit anim id est laborum."
+            </div>
           </div>
         </div>
         </>
